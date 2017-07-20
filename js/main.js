d23c7786336223527c127c3d86e8f4456439eab5
Precious Saelee <bambisaelee@gmail.com>
2017-07-20 15:50:42 -0700

    starting point of game in main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 // how to start a game
-var game = new Phaser.Game(640, 360, Phaser.Auto);
+var VisNovel = new Phaser.Game(800, 600, Phaser.Auto, '', 
+	{preload: preload, create: create, update: update});
 
 var GameState = {
 	preload: function(){
-
+		VisNovel.load.spritesheet("")
 	},
+var bg;
+var main1;
+var logo;
+var button;
+
 	create: function(){
+		VisNovel.stage.backgroundColor = "black";
 
 	},
+	function animationStarted(sprite, animation){
+	VisNovel.add.text(400,100,"K.A.P.S.", {fill: 'white'});
+
+}
+
+}
 	update: function(){
 
 	}
 };
 
-game.state.add("GameState", GameState);
-game.state.start("GameState");
+VisNovel.state.add("GameState", GameState);
+VisNovel.state.start("GameState");
+
